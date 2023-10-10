580dc3810b88f8fbdf383c967b23968d75af6991
M-Zeeshan6886 <mzxeexshuu@gamil.com>
2023-10-10 04:17:55 -0700

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,26 @@ import "./App.css";
 import { Home } from "./modules/home";
 import {AppProvider} from './components/useContext/Context';
 function App() {
-  // // const [count, setCount] = useState(0);
-  // const [wscreen, setWScreen] = useState(window.screen.width);
+  const [count, setCount] = useState(0);
+  const [wscreen, setWScreen] = useState(window.screen.width);
 
-  // const currentState = () => {
-  //   setWScreen(() => window.innerWidth);
-  // };
-  // useEffect(() => {
-  //   window.addEventListener("resize", currentState);
-  //   return () => {
-  //     window.removeEventListener("resize", currentState);
-  //   };
-  // });
+  const currentState = () => {
+    setWScreen(() => window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", currentState);
+    return () => {
+      window.removeEventListener("resize", currentState);
+    };
+  });
 
   return (
     <>
-      {/* <h1>Hlwww react im zeeshan</h1>
-      <h1>State for screen reSize {wscreen} </h1> */}
-      {/* <button onClick={() => setCount(count + 1)}>+</button>
+      <h1>Hlwww react im zeeshan</h1>
+      <h1>State for screen reSize {wscreen} </h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
       <h1>{count}</h1>
-      <button onClick={() => setCount(count - 1)}>-</button> */}
+      <button onClick={() => setCount(count - 1)}>-</button>
       <AppProvider>
         <Home />
       </AppProvider>
